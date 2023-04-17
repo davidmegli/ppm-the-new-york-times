fdcb60f9da86776f97932664bc7d6152571ae884
davidmegli <verbatimdvd91@gmail.com>
2023-04-17 20:09:24 +0200

    Finished menu modal for low res

--- scripts/left_navbar.js
+++ scripts/left_navbar.js
@@ -23,6 +23,14 @@ let GraphicsSection = getEmptySectionsByNameArray("Graphics",[]);
 let TheUpshotSection = getEmptySectionsByNameArray("The Upshot",[]);
 let MoreSection = getEmptySectionsByNameArray("More",[]);
 let MainSection = new Section("Main ", [HomePageSection, WorldSection, BusinessSection, PoliticsSection, USSection, SportsSection, HealthSection, NYSection, OpinionSection, TechSection, ScienceSection, ArtsSection, BookReviewSection, StyleSection, FoodSection, TravelSection, TSection, MagazineSection, RealEstateSection, ObituariesSection, VideoSection, GraphicsSection, TheUpshotSection, MoreSection]);
+
+let newsSectionLowRes = getEmptySectionsByNameArray("News",["Home Page","World","Coronavirus","U.S.","Politics","New York","Business","Tech","Science","Climate","Sports","Wildfire Tracker","Obituaries","The Upshot","International","Canada","Español","中文网","Today's Paper","Corrections","Trending"]);
+let opinionSectionLowRes = getEmptySectionsByNameArray("Opinion",["Today's Opinion","Columnists","Editorials","Guest Essays","Letters","Sunday Opinion","Opinion Video"]);
+let artsSectionLowRes = getEmptySectionsByNameArray("Arts",["Today's Arts","Art & Design","Books","Best Sellers Book List","Dance","Movies","Music","Pop Culture","Television","Theater","What To Watch","Video: Arts"]);
+let livingSectionLowRes = getEmptySectionsByNameArray("Living",["Automotive","Games","Education","Food","Health","Jobs","Love","Magazine","Parenting","Real Estate","Style","T Magazine","Travel","Listings & More"]);
+let listingsAndMoreSectionLowSes = getEmptySectionsByNameArray("Listings & More",["Reader Center","The Athletic","Wirecutter","Cooking","Headway","Live Events","The Learning Network","Tools & Services","Podcasts","Video","Graphics","TimesMachine","Times Store","Manage My Account","NYTLicensing"]);
+let MainSectionLowRes = new Section("Main ", [newsSectionLowRes, opinionSectionLowRes, artsSectionLowRes, livingSectionLowRes, listingsAndMoreSectionLowSes]);
+
 let navbarContainer = null;
 let menuTimeout = null;
 let submenuTimeout = null;
@@ -90,6 +98,11 @@ function createMenu() {
 
 function deleteMenu() {
     navbarContainer = document.getElementById("navbar-container");
+    let modalMenu = document.getElementById("modal-menu");
+    if(modalMenu !== null) {
+        let modal = bootstrap.Modal.getInstance(modalMenu);
+        modal.hide();
+    }
     if(navbarContainer.childNodes.length > 0)
         navbarContainer.removeChild(navbarContainer.childNodes[0]);
 }
@@ -204,9 +217,9 @@ function createLeftMenu() {
 function createLowResMenu() {
     navbarContainer = document.getElementById("navbar-container");
     //creo un menu a schermo intero, una una div in testa che contiene il logo al centro e in fondo a destra il tasto per chiudere il menu, e una div che contiene il menu vero e proprio, con in testa la barra di ricerca con id "search" e il bottone con id "search-submit" e sotto i titoli delle sezioni come h3 e le sottosezioni  come link divisi in 2 colonne
-    let menu = document.createElement("div");
+    /*let menu = document.createElement("div");
     menu.setAttribute("id", "menu-lowres");
-    menu.classList.add("fixed-top","bg-white","container-fluid","column","h-100","p-0");//,"d-none");
+    menu.classList.add("position-absolute","top-0","bg-white","container-fluid","column","h-100","p-0");//,"d-none");
     navbarContainer.appendChild(menu);
     let menuHeader = document.createElement("div");
     menuHeader.setAttribute("id", "menu-header");
@@ -238,6 +251,102 @@ function createLowResMenu() {
     menuCloseButtonIcon.style.position = "absolute";
     menuCloseButtonIcon.style.top = "20px";
     menuCloseButtonIcon.style.right = "20px";
-    menuCloseButton.appendChild(menuCloseButtonIcon);
+    menuCloseButton.appendChild(menuCloseButtonIcon);*/
+
+    let modalMenu = document.createElement("div");
+    modalMenu.setAttribute("id", "modal-menu");
+    modalMenu.classList.add("modal");
+    modalMenu.setAttribute("tabindex", "-1");
+    navbarContainer.appendChild(modalMenu);
+    let modalMenuDialog = document.createElement("div");
+    modalMenuDialog.classList.add("modal-dialog","modal-fullscreen");
+    modalMenu.appendChild(modalMenuDialog);
+    let modalMenuContent = document.createElement("div");
+    modalMenuContent.classList.add("modal-content");
+    modalMenuDialog.appendChild(modalMenuContent);
+    //Header della Modal
+    let modalMenuHeader = document.createElement("div");
+    modalMenuHeader.classList.add("modal-header");
+    modalMenuContent.appendChild(modalMenuHeader);
+    let modalMenuLogoLink = document.createElement("a");
+    modalMenuLogoLink.setAttribute("href", "#");
+    modalMenuLogoLink.setAttribute("id", "modal-menu-logo-link");
+    modalMenuLogoLink.classList.add("col-12","p-0","m-0","text-center");
+    modalMenuHeader.appendChild(modalMenuLogoLink);
+    let modalMenuLogo = document.createElement("img");
+    modalMenuLogo.setAttribute("id", "modal-menu-logo");
+    modalMenuLogo.setAttribute("alt", "New York Times Logo");
+    modalMenuLogo.setAttribute("src", "resources/NewYorkTimes.svg");
+    modalMenuLogo.classList.add("img-fluid");
+    modalMenuLogoLink.appendChild(modalMenuLogo);
+    let modalMenuCloseButton = document.createElement("button");
+    modalMenuCloseButton.setAttribute("type", "button");
+    modalMenuCloseButton.classList.add("btn-close");
+    modalMenuCloseButton.style.position = "absolute";
+    modalMenuCloseButton.style.top = "20px";
+    modalMenuCloseButton.style.right = "20px";
+    modalMenuCloseButton.setAttribute("data-bs-dismiss", "modal");
+    modalMenuCloseButton.setAttribute("aria-label", "Close");
+    modalMenuHeader.appendChild(modalMenuCloseButton);
+    //Body della Modal
+    let modalMenuBody = document.createElement("div");
+    modalMenuBody.classList.add("modal-body","column","px-5");
+    modalMenuContent.appendChild(modalMenuBody);
+    let searchContainer = document.createElement("div");
+    searchContainer.classList.add("row","bg-white","p-0","m-0","align-items-center","justify-content-center");
+    modalMenuBody.appendChild(searchContainer);
+    let modalMenuSearch = document.createElement("div");
+    modalMenuSearch.classList.add("row","bg-white","p-0","m-0","align-items-center","justify-content-center");
+    searchContainer.appendChild(modalMenuSearch);
+    let modalMenuSearchInput = document.createElement("input");
+    modalMenuSearchInput.setAttribute("id", "modal-menu-search-input");
+    modalMenuSearchInput.setAttribute("type", "text");
+    modalMenuSearchInput.setAttribute("name", "query");
+    modalMenuSearchInput.setAttribute("placeholder", "SEARCH");
+    modalMenuSearchInput.setAttribute("value", "");
+    modalMenuSearchInput.classList.add("col-10","px-2","py-3","m-0","me-2","fluid");
+    searchContainer.appendChild(modalMenuSearchInput);
+    let modalMenuSearchSubmit = document.createElement("button");
+    modalMenuSearchSubmit.setAttribute("id", "modal-menu-search-submit");
+    modalMenuSearchSubmit.setAttribute("type", "submit");
+    modalMenuSearchSubmit.setAttribute("disabled", "");
+    modalMenuSearchSubmit.classList.add("col-2","px-0","py-0","m-0","text-center");
+    modalMenuSearchSubmit.innerText = "GO";
+    searchContainer.appendChild(modalMenuSearchSubmit);
+    MainSectionLowRes.subSections.forEach((section) => {
+       let sectionDiv = document.createElement("div");
+         sectionDiv.classList.add("row","bg-white","p-0","m-0","align-items-center","justify-content-center");
+            modalMenuBody.appendChild(sectionDiv);
+            let sectionTitle = document.createElement("div");
+            sectionTitle.classList.add("row","bg-white","pt-5","pb-3","px-3","m-0","align-items-center","menu-modal-section-title","text-start");
+            sectionTitle.innerText = section.title;
+            sectionDiv.appendChild(sectionTitle);
+            let sectionSubsections = document.createElement("div");
+            sectionSubsections.classList.add("row","bg-white","p-0","px-3","m-0");
+            sectionDiv.appendChild(sectionSubsections);
+
+            section.subSections.forEach((subsection) => {
+                let subsectionLink = document.createElement("a");
+                subsectionLink.setAttribute("href", "#");
+                subsectionLink.classList.add("col-6","px-0","py-2","m-0","fluid","menu-modal-subsection");
+                subsectionLink.innerText = subsection.title;
+                sectionSubsections.appendChild(subsectionLink);
+            });
+            if (section.subSections.length % 2 !== 0) {
+                let emptyDiv = document.createElement("div");
+                emptyDiv.classList.add("col-6","px-0","py-3","m-0","text-center","fluid");
+                sectionSubsections.appendChild(emptyDiv);
+            }
+    });
+
+
+    //nelle precedenti righe News, Opinions, Arts, Living sono le sezioni principali del menu, mentre le altre sono sottosezioni corrispondenti a News, Opinions, Arts, Living, le prossime righe creani 1 div per ogni sezione, ognuna delle quali è una riga bootstrap, al cui interno ci sono 1 riga dedicata al titolo e una div divisa in 2 colonne per le sottosezioni
+
+    //Footer della Modal
+    let modalMenuFooter = document.createElement("div");
+    modalMenuFooter.classList.add("modal-footer");
+    modalMenuContent.appendChild(modalMenuFooter);
+
+
 
 }
